1634794b2e638d6e8f96dea9c22bd6c557ef4175
upendra-23-cmd <vrambharat05@icloud.com>
2026-05-29 22:02:52 +0530

    new error solved of frontend pipeline

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,9 +1,23 @@
 import js from "@eslint/js";
 import globals from "globals";
-import pluginReact from "eslint-plugin-react";
-import { defineConfig } from "eslint/config";
+import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
 
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
-  pluginReact.configs.flat.recommended,
-]);
+export default [
+  js.configs.recommended,
+  {
+    files: ["**/*.{js,jsx}"],
+    languageOptions: {
+      globals: globals.browser,
+      ecmaVersion: "latest",
+      sourceType: "module",
+    },
+    plugins: {
+      react,
+      "react-hooks": reactHooks,
+    },
+    rules: {
+      "react/react-in-jsx-scope": "off",
+    },
+  },
+];
